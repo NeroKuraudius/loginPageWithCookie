1b34cc44bf84b78598f14cfa51196e475a713314
Nero <karta0706566@gmail.com>
2023-03-02 20:25:03 +0800

    feat: add the home page

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
+require('/congif/mongoose')
+
+const app = express()
+const PORT = 3000
+
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(PORT, () => {
+  console.log(`Succeed in listening on http://localhost:${PORT}`)
+})
