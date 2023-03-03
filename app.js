eba2a3fd53513a72203da51799c0fe03cc77d6f9
Nero <karta0706566@gmail.com>
2023-03-03 19:53:56 +0800

    feat: add the login function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const User = require('./models/User.js')
 
 require('./config/mongoose')
 
@@ -12,8 +13,26 @@ app.use(express.urlencoded({ extended: true }))
 app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 
+// 登入頁面
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+// 登入
+app.post('/', (req, res) => {
+  const { email, password } = req.body
+  const warnningMessage = true
+
+  User.findOne({ email: email, password: password })
+    .lean()
+    .then(user => {
+      if (user) {
+        res.render('index', { user })
+      } else {
+        res.render('index', { warnningMessage })
+      }
+    })
+    .catch((error) => console.log(error))
 })
 
 app.listen(PORT, () => {
