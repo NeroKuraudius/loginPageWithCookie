3e133d8b403a0fec068e602f3e32392058135ef5
Nero <karta0706566@gmail.com>
2023-03-05 15:14:32 +0800

    feat: add the function of keep logining

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 const User = require('./models/User.js')
 
 require('./config/mongoose')
@@ -9,27 +10,39 @@ const app = express()
 const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.engine('hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 
+
 // 登入頁面
 app.get('/', (req, res) => {
-  res.render('index')
+  const user = req.cookies.userInfo
+
+  if (!user) {
+    return res.render('index')
+  } else {
+    return res.render('index', { user })
+  }
 })
+
 
 // 登入
 app.post('/', (req, res) => {
-  const { email, password } = req.body
+  const { email, password, keepLogin } = req.body
   const warnningMessage = true
 
   User.findOne({ email: email, password: password })
     .lean()
     .then(user => {
-      if (user) {
-        res.render('index', { user })
+      if (user && keepLogin === 'on') {
+        res.cookie('userInfo', user, { maxAge: 60000, httpOnly: true })
+        return res.render('index', { user })
+      } else if (user) {
+        return res.render('index', { user })
       } else {
-        res.render('index', { warnningMessage })
+        return res.render('index', { warnningMessage })
       }
     })
     .catch((error) => console.log(error))
